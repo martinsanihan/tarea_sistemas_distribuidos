3b9ce255febf7466f92bfbf8a0b6e1afd506bc06
AlanGK7 <alan.sotomayor@mail.udp.cl>
2026-05-29 13:52:16 -0400

    update get metricas

--- almacenador_metricas/almacenador_metricas.py
+++ almacenador_metricas/almacenador_metricas.py
@@ -40,24 +40,37 @@ def obtener_resumen():
     if not os.path.exists(ARCHIVO_LOG):
         return jsonify({"error": "No hay métricas aún"})
     
-
     df = pd.read_csv(ARCHIVO_LOG)
-    
-  
     df['tipo_evento'] = df['tipo_evento'].astype(str).str.strip().str.upper()
     
     total = len(df)
-   
     hits = len(df[df['tipo_evento'] == 'HIT'])
     misses = len(df[df['tipo_evento'] == 'MISS'])
     
+    #  nuevas metricas
+    retries = len(df[df['tipo_evento'] == 'RETRY'])
+    recoveries = len(df[df['tipo_evento'] == 'RECOVERY'])
+    dlq = len(df[df['tipo_evento'] == 'DLQ'])
+    
+    # calculo de percentiles (p50 y p95) solo para consultas exitosas (HIT/MISS)
+    df_exitosas = df[df['tipo_evento'].isin(['HIT', 'MISS'])]
+    if not df_exitosas.empty:
+        p50 = round(df_exitosas['latencia_ms'].quantile(0.50), 2)
+        p95 = round(df_exitosas['latencia_ms'].quantile(0.95), 2)
+    else:
+        p50, p95 = 0, 0
+        
     return jsonify({
         "total_peticiones": total,
         "hits": hits,
         "misses": misses,
-        "hit_rate_actual": round(hits / total, 4) if total > 0 else 0
+        "hit_rate": round(hits / total, 4) if total > 0 else 0,
+        "retry_rate": round(retries / total, 4) if total > 0 else 0,
+        "recovery_rate": round(recoveries / total, 4) if total > 0 else 0,
+        "dlq_rate": round(dlq / total, 4) if total > 0 else 0,
+        "latencia_p50_ms": p50,
+        "latencia_p95_ms": p95
     })
-
 if __name__ == '__main__':
     print("Iniciando Almacenador de Métricas corregido en puerto 6000...")
     app.run(host='0.0.0.0', port=6000)
